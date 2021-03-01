726eb590d55c4f327814d02af945af28a077fca6
xyovanz <xyovanz@users.noreply.github.com>
2021-03-01 16:15:26 +0300

    lesson1

---
+++ Lesson1/Asteroids/SmallStar.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Drawing;
+
+namespace Asteroids
+{
+    class SmallStar : Template
+    {
+        public SmallStar(Point pos, Point dir, Size size) : base(pos, dir, size)
+        {
+        }
+
+        public override void Draw()
+        {
+            Game.Buffer.Graphics.FillRectangle(Brushes.White, Pos.X, Pos.Y, Size.Width, Size.Height);
+        }
+
+        public override void Update()
+        {
+            Pos.X = Pos.X + Dir.X;
+            if (Pos.X < 0 - Size.Width)
+            {
+                Pos.X = Game.Width + Size.Width;
+                Pos.Y = Convert.ToInt32(random.NextDouble() * (double)Game.Height);
+            }
+        }
+    }
+}
---
+++ Lesson1/Asteroids/Star.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace Asteroids
+{
+    class Star : Template
+    {
+        List<Bitmap> bitMapList = new List<Bitmap>() { new Bitmap("..\\..\\img/stars/star1.png"),
+            new Bitmap("..\\..\\img/stars/star2.png"), new Bitmap("..\\..\\img/stars/star3.png"),
+            new Bitmap("..\\..\\img/stars/star4.png"), new Bitmap("..\\..\\img/stars/star5.png"),
+            new Bitmap("..\\..\\img/stars/star6.png"), new Bitmap("..\\..\\img/stars/star7.png") };
+
+        int starsForm = 0;
+        
+        public Star(Point pos, Point dir, Size size) : base(pos, dir, size)
+        {
+        }
+        
+        public override void Draw()
+        {
+            starsForm++;
+            if (starsForm == bitMapList.Count)
+                starsForm = 0;
+            Game.Buffer.Graphics.DrawImage(bitMapList[starsForm], Pos.X, Pos.Y, Size.Width, Size.Height);
+        }
+        
+        public override void Update()
+        {
+            Pos.X = Pos.X + Dir.X;
+            if (Pos.X < 0 - Size.Width) Pos.X = Game.Width + Size.Width;
+
+        }
+    }
+}
